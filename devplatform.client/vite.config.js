93537837ec17972b353daf4ce0bc2381c96c6d85
RamazonFarmanov <romafarmanov07@gmail.com>
2025-03-01 12:21:00 +0500

    Docker added but it doesn't work

--- devplatform.client/vite.config.js
+++ devplatform.client/vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
 import fs from 'fs';
@@ -7,31 +6,11 @@ import path from 'path';
 import child_process from 'child_process';
 import { env } from 'process';
 
-const baseFolder =
-    env.APPDATA !== undefined && env.APPDATA !== ''
-        ? `${env.APPDATA}/ASP.NET/https`
-        : `${env.HOME}/.aspnet/https`;
+const certPath = "certs/devplatform.client.pem";
+const keyPath = "certs/devplatform.client.key";
 
-const certificateName = "devplatform.client";
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
-
-if (!fs.existsSync(baseFolder)) {
-    fs.mkdirSync(baseFolder, { recursive: true });
-}
-
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-    if (0 !== child_process.spawnSync('dotnet', [
-        'dev-certs',
-        'https',
-        '--export-path',
-        certFilePath,
-        '--format',
-        'Pem',
-        '--no-password',
-    ], { stdio: 'inherit', }).status) {
-        throw new Error("Could not create certificate.");
-    }
+if (!fs.existsSync(certPath) || !fs.existsSync(keyPath)) {
+    throw new Error("SSL сертификаты не найдены в контейнере!");
 }
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
@@ -54,8 +33,8 @@ export default defineConfig({
         },
         port: 58604,
         https: {
-            key: fs.readFileSync(keyFilePath),
-            cert: fs.readFileSync(certFilePath),
+            key: fs.readFileSync(keyPath),
+            cert: fs.readFileSync(certPath),
         }
     }
 })
